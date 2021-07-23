65177669efbc69f47d746e6760b63c7871f3af25
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-07-23 13:01:31 -0300

    Incluindo arquivos

---
+++ horaDoDia/javaScript.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia 
+        img.src = '/img/fotoManha.png'
+        document.body.style.background = '#F97F15'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = '/img/fotoTarde.png'
+        document.body.style.background = '#c05f1c'
+    } else {
+        //Boa noite
+        img.src = '/img/fotoNoite.png'
+        document.body.style.background = '#09182e'
+    }
+}
---
+++ verificadorDeIdade/javaScript.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtAno')
+    var res = document.querySelector('div#res')
+    var img = document.getElementById('imagem')
+
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        //var img = document.createElement('img')
+        //img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                //img.setAttribute('src', '/ex015/img/fotoCriancaMasc.png')
+                img.src = 'img/foto-Crianca-Masc.png'
+                document.body.style.background = '#F97F15'
+            } else if (idade < 21) {
+                //Jovem
+                img.src = '/img/fotoCriancaMasc.png'
+            } else if (idade < 50) {
+                //adulto
+                img.src = '/img/fotoCriancaMasc.png'
+            } else {
+                //idoso
+                img.src = '/img/fotoCriancaMasc.png'
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+        img.setAttribute('src', 'fotoCriancaMasc.png')
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos.`
+    }
+}
