56b2661afc940e933414b0fb4fe36517e188e83a
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-07-23 21:30:19 -0300

    main

--- horaDoDia/javaScript.js
+++ horaDoDia/javaScript.js
@@ -7,15 +7,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas!`
     if (hora >= 0 && hora < 12) {
         //Bom dia 
-        img.src = '/img/fotoManha.png'
+        img.src = '/horaDoDia/img/fotoManha.png'
         document.body.style.background = '#F97F15'
     } else if (hora >= 12 && hora <= 18) {
         //Boa tarde
-        img.src = '/img/fotoTarde.png'
+        img.src = '/horaDoDia/img/fotoTarde.png'
         document.body.style.background = '#c05f1c'
     } else {
         //Boa noite
-        img.src = '/img/fotoNoite.png'
+        img.src = '/horaDoDia/img/fotoNoite.png'
         document.body.style.background = '#09182e'
     }
 }
